a4c88b7f2b112b4cc5bb5d643f838a612397920d
Syed Muhammad Hasan Albiz <albizsyed@gmail.com>
2025-05-10 20:14:59 -0400

    function should return relevant information

---
+++ src/analysis/index.ts
@@ -0,0 +1,23 @@
+import Papa from 'papaparse';
+
+export function csvParse(csvString: string) {
+    var parsedCsv = Papa.parse(csvString);
+    var relevantRows: string[][] = [];
+    for(let i = 0; i < parsedCsv.length; i++){
+        var isRelevant = true;
+        var row: string[] = parsedCsv[i];
+        var relevantCols: string[] = [];
+        for(let k = 0; k <= 3; k++){
+            if (row[k] != null){
+                isRelevant = false;
+            }
+            relevantCols.push(row[k]);
+        }
+        if (isRelevant == true){
+            relevantRows.push(relevantCols)
+        }
+    }
+
+    console.table(relevantRows);
+    return relevantRows;
+}
